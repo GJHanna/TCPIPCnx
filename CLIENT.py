48740d17f78d3cca80552af7a928af9a529b7a9f
GeorgeHanna <ghanna610@gmail.com>
2020-07-07 18:41:27 +0300

    Initial

---
+++ CLIENT.py
@@ -0,0 +1,92 @@
+import binascii
+from argparse import ArgumentParser
+import socket
+
+# Create command-line arguments 
+# ip is a must while port is optional with a default value of 6653
+# Usage: in the cmd run Python run one of the following
+# 'python CLIENT.py 127.0.0.1' <- IP=127.0.0.1, Port=6653
+# 'python CLIENT.py 127.0.0.1 653' <- IP=127.0.0.1, Port=653
+parser = ArgumentParser()
+parser.add_argument('ip', 
+                    help='IP Address')
+parser.add_argument('port', 
+                    nargs='?', 
+                    type=int, 
+                    default=6653, 
+                    help='Port number')
+
+class PortNumberError(Exception):
+    pass
+
+class CLIENT(object):
+    def __init__(self, ip, port):
+        try:
+            i = ip.split('.') # 0.0.0.0 becomes an array: [0, 0, 0, 0]
+
+            # Validate IP address
+            if (len(i) != 4):
+                raise ValueError
+            for num in i:
+                if (int(num) < 0 or int(num) > 255):
+                    raise ValueError
+                
+            # Validate port number 
+            if (port < 0 or port > 65535):
+                raise PortNumberError
+        except ValueError:
+            exit('Invalid IP address format. IP address should be between \
+                0.0.0.0 and 255.255.255.255')
+        except PortNumberError:
+            exit('Invalid port number. Port number should be from 0 to 65535.')
+        except Exception:
+            exit('IP address should be between 0.0.0.0 and 255.255.255.255. \
+                Port number should be from 0 to 65535')
+        finally:
+            self.__host = ip
+            self.__port = port
+            self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__sock.settimeout(5)
+    
+    def connect(self):
+        try:
+            self.__sock.connect((self.__host, self.__port)) # Connect to IP
+            print('Connected to {} through port {}'.format(self.__host,
+                self.__port))
+        except ConnectionRefusedError:
+            exit('Connection refused.')
+        except socket.timeout:
+            exit('Connection timeout')
+        except ConnectionError:
+            exit('Connection unreachable.')
+
+    def send(self):
+        try:
+            # Send data to server from Hex to bytes
+            print(bytes.fromhex('F000001400'))
+            # self.__sock.send(bytes.fromhex('F000001400'))
+            # self.__sock.send(bytes('F000001400'.encode('utf-8')))
+            # self.__sock.send('F000001400'.encode('utf-8'))
+            # self.__sock.send(bytearray.fromhex('F000001400'))
+            # self.__sock.send(binascii.unhexlify('F000001400'))
+            data = 'F000001400'
+            self.__sock.send(bytes.fromhex(data))
+        except ConnectionRefusedError:
+            exit('Connection refused.')
+
+    def receive(self):
+        # Received data from server with buffer of 512
+        bytes_recvd = self.__sock.recv(1024)
+        
+        if (bytes_recvd == b''):
+            raise ValueError('No data received')
+        print('Data received: ' + str(bytes_recvd[0]) + ' ' + \
+            str(bytes_recvd[1]))
+        return bytes_recvd 
+
+if __name__=='__main__':
+    args = parser.parse_args()
+    cnx = CLIENT(args.ip, args.port)
+    cnx.connect()
+    cnx.send()
+    cnx.receive()
---
+++ SERVER.py
@@ -0,0 +1,18 @@
+
+from time import sleep
+from sys import getsizeof
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1024))
+print('Server is running...')
+s.listen(5)
+
+while True:
+    clt, adr = s.accept()
+    print(f'Connection to {adr} established')
+    r = clt.recv(512)
+    if r == bytes.fromhex('F000001400'):
+        clt.send(bytes.fromhex('deadbeef'))
+
+clt.close()
